c60c7be8a4f44bc38c7b9dd367ca4563cf9e5cf0
Loan <minege02@gmail.com>
2017-06-20 10:46:01 +0200

    Updated debug

--- _eSSP/init.c
+++ _eSSP/init.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-SSP_COMMAND *ssp_init(char *port_c, char *addr_c, bool debug)
+SSP_COMMAND *ssp_init(char *port_c, char *addr_c, int debug)
 {
     SSP_COMMAND* sspC = malloc(sizeof(SSP_COMMAND));
     
@@ -16,7 +16,7 @@ SSP_COMMAND *ssp_init(char *port_c, char *addr_c, bool debug)
     sspC->BaudRate = 9600;
     sspC->Key.EncryptKey = 1;   // Just to make sure 
     //Open the COM port
-    if ( debug == true ){ 
+    if ( debug == 1 ){ 
 	    printf("PORT: %s\n", port_c); // Printing the connection informations
 
 	    printf ("##%s##\n", port_c);
